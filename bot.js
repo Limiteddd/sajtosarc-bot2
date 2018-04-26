b3b0dc03f9a7ed82a51ce11aee90f6d415791b34
Limiteddd <38568217+Limiteddd@users.noreply.github.com>
2018-04-26 19:38:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,6 +8,8 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === 'mérges') {
     	message.reply('Mérges?');
+         if (message.content === 'autista') {
+    	message.reply('Autista?');
   	}
 });
 
